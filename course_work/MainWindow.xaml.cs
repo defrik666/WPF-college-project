0aa9b70460c1ec960cbcb8d64bf596487e338836
defrik666 <66868337+defrik666@users.noreply.github.com>
2023-11-02 11:57:18 +0300

    1

--- course_work/MainWindow.xaml.cs
+++ course_work/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +17,32 @@ using System.Windows.Shapes;
 
 namespace course_work
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            testGrid.ItemsSource = LoadData();
+
+        }
+
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("server=DESKTOP-8IUIQUA\\SQLEXPRESS;Trusted_Connection=Yes;DataBase=ErmakovProject;");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            return dataTable;
+        }
+
+        public DataView LoadData()
+        {
+            return Select("Select * FROM Rooms").DefaultView;
         }
     }
 }
